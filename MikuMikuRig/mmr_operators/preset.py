697d0fa80fbe7dde464844740bc6456737a934bf
小威廉伯爵 <958261649@qq.com>
2023-02-03 20:01:24 +0800

    合并了mmr骨骼属性

--- MikuMikuRig/mmr_operators/preset.py
+++ MikuMikuRig/mmr_operators/preset.py
@@ -3,6 +3,8 @@ import bpy
 import os
 import json
 from . import rig
+from bpy.props import BoolProperty,IntProperty,FloatProperty,EnumProperty,StringProperty
+
 
 #骨骼枚举属性部分
 
@@ -76,21 +78,43 @@ bone_translate_dict_C={
 built_in_rig_dict_list=['None','MMD_JP','MMD_EN','VRoid']
 built_in_retarget_dict_list=['None','mixamo','Rigify','FBX动捕','BVH动捕']
 
-bpy.types.PoseBone.mmr_bone_invert=bpy.props.BoolProperty(
+#mmr骨骼属性类
+class MMR_bone(bpy.types.PropertyGroup):
+    bone_type:StringProperty(description=('Choose the bone type you want to use'))
+    invert:BoolProperty(default=False)
+    mass:FloatProperty(default=0,description="bone mass",min=0)
+
+#注册mmr骨骼属性
+bpy.utils.register_class(MMR_bone)
+bpy.types.PoseBone.mmr_bone = bpy.props.PointerProperty(type=MMR_bone)
+'''
+#定义骨骼头尾是否反转
+bpy.types.PoseBone.mmr_bone_invert=BoolProperty(
     default=False
 )
 
-'''bpy.types.PoseBone.mmr_bone_type=bpy.props.EnumProperty(
+bpy.types.PoseBone.mmr_bone_type=bpy.props.EnumProperty(
         items=[
             (name, name, '') for name in bone_type_list
         ],
         description=('Choose the bone type2 you want to use'),
-    )'''
+    )
+
+#定义骨骼类型
 #预设属性改为字符串
-bpy.types.PoseBone.mmr_bone_type=bpy.props.StringProperty(
+bpy.types.PoseBone.mmr_bone_type=StringProperty(
         description=('Choose the bone type2 you want to use'),
     )
 
+#定义骨骼质量
+bpy.types.PoseBone.mmr_bone_mass=FloatProperty(
+    default=1,
+    description="bone mass"
+    ,min=0
+    )
+'''
+mmr_bone_property_list=[name for name in MMR_bone.__annotations__.keys()]
+mmr_bone_property_set=set(mmr_bone_property_list)
 #骨架枚举属性部分
 
 my_dir = os.path.dirname(os.path.realpath(__file__))
@@ -107,10 +131,14 @@ preset_dict_dict={
 
 
 def get_preset(pose):
-    preset={}
+    preset={
+        'interpretation':mmr_bone_property_list,
+    }
+    data={}
     for bone in pose.bones:
-        if bone.mmr_bone_type!='':
-            preset[bone.name]=(bone.mmr_bone_type,bone.mmr_bone_invert)
+        value_list=[value for prop_name,value in bone.mmr_bone.items()]
+        if value_list[0]:
+            data[bone.name]=value_list
     return(preset)
 
 def set_bone_type(pose,preset):
